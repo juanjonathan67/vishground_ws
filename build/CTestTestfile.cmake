f0ed7d875e716669f914110e59dd10b6673dbb53
juanjonathan67 <juanjonathan67>
2023-08-24 19:31:06 +0700

    Added .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntuihiw/vishground_ws/src
-# Build directory: /home/ubuntuihiw/vishground_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("vishground")
--- build/vishground/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/ubuntuihiw/vishground_ws/src/vishground/include".split(';') if "/home/ubuntuihiw/vishground_ws/src/vishground/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "geometry_msgs;mavros;mavros_msgs;nav_msgs;roscpp;roslaunch;rospy;std_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lvishground".split(';') if "-lvishground" != "" else []
-PROJECT_NAME = "vishground"
-PROJECT_SPACE_DIR = "/home/ubuntuihiw/vishground_ws/devel"
-PROJECT_VERSION = "0.0.0"
